df220f3d672312710ffff7a2ca065637ad860929
Florin <florin.patrascu@gmail.com>
2019-05-12 14:43:40 -0400

    prepp'in for publishing

--- mix.exs
+++ mix.exs
@@ -1,13 +1,16 @@
 defmodule Fixex.MixProject do
   use Mix.Project
 
+  @url_github "https://github.com/florinpatrascu/fixex"
+
   def project do
     [
       app: :fixex,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -27,5 +30,20 @@ defmodule Fixex.MixProject do
       {:dialyxir, "1.0.0-rc.6", only: [:dev], runtime: false},
       {:mix_test_watch, "~> 0.9", only: [:dev, :test]}
     ]
+  end
+
+  defp package do
+    %{
+      files: [
+        "lib",
+        "mix.exs",
+        "LICENSE"
+      ],
+      licenses: ["Apache 2.0"],
+      maintainers: ["Florin T.PATRASCU"],
+      links: %{
+        "Github" => @url_github
+      }
+    }
   end
 end
